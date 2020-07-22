dc6014d4abde984b096ba85b5d0393ff260a7d09
tomopict <tomo.cat.picture@gmail.com>
2020-07-22 23:33:22 +0900

    add basebutoon

---
+++ src/components/atoms/baseButton.vue
@@ -0,0 +1,18 @@
+<template>
+  <button class="button">
+    <slot />
+  </button>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({})
+</script>
+
+<style scoped>
+button {
+  padding: 10px;
+  border: 1px solid #000;
+  border-radius: 5px;
+}
+</style>
