26ecc7ba20ceac07a6cfbffd6eee54f04ecea44b
tomopict <tomo.cat.picture@gmail.com>
2020-08-02 12:37:14 +0900

    async awaitに変更

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -20,19 +20,20 @@ const client = new line.Client({
 // Start writing Firebase Functions
 // https://firebase.google.com/docs/functions/typescript
 
-function handleClientReply(
+async function handleClientReply(
   replyToken: string,
   tmpMessage: tempMessage,
   response: functions.Response
 ) {
-  client
-    .replyMessage(replyToken, tmpMessage)
-    .then(() => {
-      response.status(200).send('OK')
-    })
-    .catch((err: any) => {
-      console.log('Error getting document', err)
-    })
+  try {
+    await client.replyMessage(replyToken, tmpMessage)
+    response.status(200).send('OK')
+  } catch (e) {
+    response.status(400).send('NG')
+    console.log('Error getting document', e)
+  } finally {
+    console.log('done', tmpMessage)
+  }
 }
 const sendPushNotification = function (
   token: string,
@@ -75,12 +76,18 @@ export const createMessage = functions
     } else if (!change.after.exists) {
       console.log('削除された')
       message = change.before.data()
+      if (!message.name) {
+        message.name = '名無し'
+      }
       receiverRef = firestore.collection('users').doc(message.name)
       senderName = message.name
       content = `${message.title}を買ったみたい!`
     } else {
       console.log('作成された', change.after.data())
       message = change.after.data()
+      if (!message.name) {
+        message.name = '名無し'
+      }
       receiverRef = firestore.collection('users').doc(message.name)
       senderName = message.name
       content = `${message.title}を欲しいみたい!`
@@ -121,112 +128,111 @@ export const createMessage = functions
           console.log('notExists')
         }
       })
-      .catch((error: any) => {
-        console.log('errorがおきました: ', error)
+      .catch((e: any) => {
+        console.log('errorがおきました: ', e)
       })
   })
 
 // https://ruller.lsv.jp/?p=72
 exports.line = functions
   .region('asia-northeast1')
-  .https.onRequest((request, response) => {
+  .https.onRequest(async (request, response) => {
     const body = request.body
     const events = body.events
+    const shoppingRef = firestore.collection('shoppinglist')
 
     for (let event of events) {
       const { replyToken, type, message } = event
-      let tmpMessage: tempMessage
       let tmpText: string = ''
       if (type == 'message') {
         if (message.type == 'text') {
-          const regexWant = /ほしいもの|欲しいもの/g
-          const regexCreate = /追加/g
-          const regexDelete = /買った/g
+          let tmpMessage: tempMessage
+          const regex = {
+            want: /欲しいもの|ほしいもの/g,
+            create: /欲しい|ほしい|追加/g,
+            delete: /買った|かった|削除/g,
+          }
 
-          if (message.text.match(regexWant)) {
-            const shoppingRef = firestore.collection('shoppinglist')
+          if (message.text.match(regex.want)) {
+            try {
+              const querySnapshot = await shoppingRef
+                .where('display', '==', true)
+                .get()
 
-            shoppingRef
-              .where('display', '==', true)
-              .get()
-              .then((querySnapshot: any) => {
-                response.status(200).send('OK')
-                console.info(querySnapshot.size)
-
-                if (!querySnapshot.size) {
-                  tmpText = '今は何もないみたい!'
-                } else {
-                  querySnapshot.forEach((doc: any) => {
-                    tmpText += `${doc.data().title}\n`
-                  })
-                  tmpText += 'が欲しいみたいだよ!'
-                  tmpMessage = {
-                    type: 'text',
-                    text: tmpText,
-                  }
-                }
-
-                handleClientReply(replyToken, tmpMessage, response)
-              })
-              .catch((err: any) => {
-                response.status(400).send('NG')
-                console.log('Error getting document', err)
-              })
-          } else if (message.text.match(regexCreate)) {
-            const shoppingRef = firestore.collection('shoppinglist')
-            const title = message.text.replace(regexCreate, '')
+              if (!querySnapshot.size) {
+                tmpText = '今は何もないみたい!'
+              } else {
+                querySnapshot.forEach((doc: any) => {
+                  tmpText += `${doc.data().title}\n`
+                })
+                tmpText += 'が欲しいみたいだよ!'
+              }
+            } catch (err) {
+              response.status(400).send('NG')
+              console.error('Error getting document: ', err)
+              tmpText = 'エラーがおきたみたい!'
+            } finally {
+              tmpMessage = {
+                type: 'text',
+                text: tmpText,
+              }
+            }
+            await handleClientReply(replyToken, tmpMessage, response)
+          } else if (message.text.match(regex.create)) {
+            const title = message.text.replace(regex.create, '')
             const data = {
               title: title,
               name: '名無し',
               timestamp: admin.firestore.FieldValue.serverTimestamp(),
               display: true,
             }
-            shoppingRef
-              .add(data)
-              .then(function () {
-                console.log('Document written with ID:')
-                response.status(200).send('OK')
-              })
-              .catch(function () {
-                console.error('Error adding document: ')
-                response.status(400).send('NG')
-              })
-          } else if (message.text.match(regexDelete)) {
-            console.log('削除したい')
+            try {
+              await shoppingRef.add(data)
+              console.log('Document written with ID')
+              response.status(200).send('Document written with ID')
+            } catch (err) {
+              console.error('Error written document: ', err)
+              response.status(500).send('Error written document')
+            } finally {
+              console.log('Done created')
+            }
+          } else if (message.text.match(regex.delete)) {
+            const title = message.text.replace(regex.delete, '')
 
-            const shoppingRef = firestore.collection('shoppinglist')
-            const title = message.text.replace(regexDelete, '')
+            try {
+              const querySnapshot = await shoppingRef
+                .where('title', '==', title)
+                .get()
 
-            shoppingRef
-              .where('title', '==', title)
-              .get()
-              .then((querySnapshot: any) => {
-                querySnapshot.forEach((doc: any) => {
-                  console.log(doc.id)
-                  shoppingRef
-                    .doc(doc.id)
-                    .delete()
-                    .then(function () {
-                      response.status(200).send('OK')
-                      console.log('Document successfully deleted!')
-                    })
-                    .catch(function (error: any) {
-                      response.status(400).send('NG')
-                      console.error('Error removing document: ', error)
-                    })
+              if (querySnapshot.size) {
+                querySnapshot.forEach(async (doc: any) => {
+                  try {
+                    await shoppingRef.doc(doc.id).delete()
+                    response.status(200).send('Document successfully deleted')
+                    console.log('Document successfully deleted!')
+                  } catch (err) {
+                    response.status(500).send('Error removing document')
+                    console.error('Error removing document: ', err)
+                  } finally {
+                    console.log('Done deleted')
+                  }
                 })
-              })
-              .catch(() => {
-                console.error('Error adding document: ')
-                response.status(400).send('NG')
-              })
+              } else {
+                response.status(404).send('404 Not Found')
+              }
+            } catch (err) {
+              console.error('Error delete document: ', err)
+              response.status(400).send('NG')
+            } finally {
+              console.log('Done deleted')
+            }
           } else {
             tmpMessage = {
               type: 'text',
               text: message.text,
             }
 
-            handleClientReply(replyToken, tmpMessage, response)
+            await handleClientReply(replyToken, tmpMessage, response)
           }
         }
       }
