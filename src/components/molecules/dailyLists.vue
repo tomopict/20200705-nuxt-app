9447758d55c34b0df772ead359999f4772665926
tomopict <tomo.cat.picture@gmail.com>
2020-08-05 22:06:48 +0900

    日用品用のコンポーネントを追加

---
+++ src/components/molecules/dailyLists.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="border p-2 mb-5">
+    <p>いつもの</p>
+    <ul class="daily-lists">
+      <li v-for="list in dailyLists" :key="list.value">
+        <label class="switch">
+          <input
+            type="checkbox"
+            v-model="list.status"
+            @change="handleChangeDailyItemStatus(list.value, list.status)"
+          />
+          {{ list.label }}
+        </label>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  props: {
+    dailyLists: {
+      type: Array,
+      required: false,
+    },
+  },
+  methods: {
+    async handleChangeDailyItemStatus(
+      value: string,
+      status: boolean
+    ): Promise<void> {
+      const db = await this.$firebase.firestore()
+      const data = {
+        status,
+      }
+      try {
+        db.collection('dailynecessaries').doc(value).update(data)
+        console.log('Update success')
+      } catch (err) {
+        console.log('Update error', err)
+      }
+      console.log('handleChangeDailyItemStatus')
+    },
+  },
+})
+</script>
+<style lang="scss" scoped>
+.daily {
+  &-lists {
+    display: grid;
+    grid-template-columns: 33% 33% 1fr;
+  }
+}
+</style>
