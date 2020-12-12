7a1864b2fe8411167036625ad30508b7d4ba7fb3
tomopict <tomo.cat.picture@gmail.com>
2020-12-12 14:03:17 +0900

    functions内のNuxt用のfunctionを削除

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -2,10 +2,8 @@ import * as functions from 'firebase-functions'
 const admin = require('firebase-admin')
 admin.initializeApp(functions.config().firebase)
 
-const ssr = require('./ssr')
 const line = require('./line')
 const createMessage = require('./create-message')
 
-exports.ssr = ssr.ssr
 exports.line = line.line
 exports.createMessage = createMessage.createMessage
